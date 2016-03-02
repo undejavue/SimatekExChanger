2887ccb9ba01ff440f6b36408a594380c8c5cd4e
undejavue <undejavue@gmail.com>
2016-03-02 22:59:37 +0300

    Commit

---
+++ WPFinterface/Converters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+
+
+namespace WPFinterface
+{
+    public static class Converters
+    {
+
+        public static LogEntryCollectionToTextConverter LogEntryCollectionToTextConverter = new LogEntryCollectionToTextConverter();
+      
+    }
+    public class LogEntryCollectionToTextConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            ObservableCollection<string> logEntries = values[0] as ObservableCollection<string>;
+
+            if (logEntries != null && logEntries.Count > 0)
+                return logEntries.ToString();
+            else
+                return String.Empty;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
